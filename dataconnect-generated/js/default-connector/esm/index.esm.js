336a956298d7979055e71f549d905f85c9b24fa3
takakowatabe <kikotkk@gmail.com>
2025-02-19 08:37:59 +0900

    firebase設定

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,8 @@
+import { , validateArgs } from 'firebase/data-connect';
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'study-app',
+  location: 'us-central1'
+};
+
--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-
+import React from 'react';
 import "./App.css";
 import { supabase } from "./supabaseClient.js";
 import { useState, useEffect } from "react";
